7f44a68cc0c7bb7ecc9688aa8854e607c24beee2
icoicounday <75007893+icoicounday@users.noreply.github.com>
2020-12-27 19:38:36 +0900

    共通した記述beforeを導入

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,16 +1,19 @@
 require 'rails_helper'
-
 RSpec.describe User, type: :model do
-  describe "ユーザー新規登録" do
-    it "nameが空では登録できない" do
-      user = User.new(name: "", email: "test@example", password: "000000", password_confirmation: "000000")
-      user.valid?
-      expect(user.errors.full_messages).to include("Name can't be blank")
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  describe 'ユーザー新規登録' do
+    it "nameが空だと登録できない" do
+      @user.name = ""
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Name can't be blank"
     end
     it "emailが空では登録できない" do
-      user = User.new(name: "test", email: "", password: "000000", password_confirmation: "000000")
-      user.valid?
-      expect(user.errors.full_messages).to include("Email can't be blank")
+      @user.email = ""
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Email can't be blank"
     end
   end
 end

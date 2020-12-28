7a7f99694596a3406badc567d784401c685d1e4b
icoicounday <75007893+icoicounday@users.noreply.github.com>
2020-12-28 11:49:00 +0900

    factories/user.rbに新規登録の項目を記載

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,13 @@
 FactoryBot.define do
   factory :user do
-    name              {"test"}
-    email                 {"test@example"}
-    password              {"000000"}
+    name                  {Faker::Name.initials(number: 6)}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
+    last_name
+    first_name
+    last_name_kana
+    first_name_kana
+    birthday
   end
 end

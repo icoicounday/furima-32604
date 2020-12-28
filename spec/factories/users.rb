626089818e1de9b3b397dad6679652cb1747f164
icoicounday <75007893+icoicounday@users.noreply.github.com>
2020-12-28 12:44:56 +0900

    chatappのユーザー情報をFactoryBotで指定

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,9 @@
 FactoryBot.define do
   factory :user do
-    name                  {Faker::Name.initials(number: 6)}
-    email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6)}
+    name {Faker::Name.last_name}
+    email {Faker::Internet.free_email}
+    password = Faker::Internet.password(min_length: 6)
+    password {password}
     password_confirmation {password}
     last_name
     first_name

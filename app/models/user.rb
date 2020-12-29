046e2ca2c259f7047375ca8f5acec8d2ea32c0d8
icoicounday <75007893+icoicounday@users.noreply.github.com>
2020-12-29 18:10:05 +0900

    修正依頼3バリデーションを見やすくした

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+         with_options presence: true do
+           validates :name
 
-         validates :name, presence: true
-         validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
-         validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
-         validates :first_name_kana, presence: true, format: { with: /\A[ァ-ン]+\z/ }
-         validates :last_name_kana, presence: true, format: { with: /\A[ァ-ン]+\z/ }
-         validates :password, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/ }
+           with_options format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ } do
+            validates :first_name
+            validates :last_name
+          end
+
+          with_options format: { with: /\A[ァ-ン]+\z/ } do
+            validates :first_name_kana
+            validates :last_name_kana
+          end
+          
+         validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/ }
+         end
 end

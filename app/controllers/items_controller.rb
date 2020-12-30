09ac9f4e18b4b727cdad90a4731ed6d9eb361d7b
icoicounday <75007893+icoicounday@users.noreply.github.com>
2020-12-30 15:10:32 +0900

    クラスを記述しリンクを作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,20 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+
+  # def create
+  #   @item = @room.messages.new(message_params)
+  #   @item.save
+  # end
+
+  # private
+
+  # def message_params
+  #   params.require(:message).permit(:content).merge(user_id: current_user.id)
+  # end
 end

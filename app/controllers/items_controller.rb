cf74b59e09f857019038b6e65682e460b7dd0bb8
icoicounday <75007893+icoicounday@users.noreply.github.com>
2020-12-30 16:02:54 +0900

    DBに値を保存できるように、createアクションを実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,14 +8,14 @@ class ItemsController < ApplicationController
   end
 
 
-  # def create
-  #   @item = @room.messages.new(message_params)
-  #   @item.save
-  # end
+  def create
+    @item = @user.items.new(item_params)
+    @item.save
+  end
 
-  # private
+  private
 
-  # def message_params
-  #   params.require(:message).permit(:content).merge(user_id: current_user.id)
-  # end
+  def item_params
+    params.require(:item).permit(:content).merge(user_id: current_user.id)
+  end
 end

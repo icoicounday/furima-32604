8671c5fa1894384842e660bc7453a59d8f3e0ba6
icoicounday <75007893+icoicounday@users.noreply.github.com>
2021-01-04 20:25:44 +0900

    アソシエーションを記載

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,14 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
   has_one_attached :image
+
+
+
+  belongs_to :category
+  belongs_to :area
+  belongs_to :condition
+  belongs_to :pay
+  belongs_to :day
+
 end

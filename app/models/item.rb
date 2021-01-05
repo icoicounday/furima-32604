039c259350251f43d11a9fe1bb14b0c97fcc22e3
icoicounday <75007893+icoicounday@users.noreply.github.com>
2021-01-05 18:25:47 +0900

    presence: true,を削除、各active_hashモデルに対するアソシエーション も追記

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,14 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
   has_one_attached :image
+  has_one_attached :name
+  has_one_attached :text
+  has_one_attached :category_id
+  has_one_attached :condition_id
+  has_one_attached :pay_id
+  has_one_attached :area_id
+  has_one_attached :day_id
+  has_one_attached :price
   
   with_options presence: true do
     validates :name, presence: true
@@ -11,6 +19,6 @@ class Item < ApplicationRecord
     validates :pay_id
     validates :area_id
     validates :day_id
-    validates :price, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
+    validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
   end
 end

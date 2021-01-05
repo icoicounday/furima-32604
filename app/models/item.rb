d6c3e0cec11fd0a16ffc69d49761d6f2d6dfbde5
icoicounday <75007893+icoicounday@users.noreply.github.com>
2021-01-05 21:59:01 +0900

    修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,11 +11,15 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :name, presence: true
     validates :text, presence: true
+  end
+
+  with_options numericality: { other_than: 1 }do
     validates :category_id
     validates :condition_id
     validates :pay_id
     validates :area_id
     validates :day_id
-    validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
   end
+
+  validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -31,76 +31,81 @@ RSpec.describe Item, type: :model do
       end
 
       it "カテゴリーの情報がないとアイテムは保存できない" do
-        @item.category_id = ""
+        @item.category_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Category can't be blank")
+        expect(@item.errors.full_messages).to include("Category must be other than 1")
       end
       it "・・・カテゴリーの情報は1以外でないと登録できない" do
-        @item.category_id = "1"
+        @item.category_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Category can't be blank")
+        expect(@item.errors.full_messages).to include("Category must be other than 1")
       end
 
+
+
       it "商品の状態の情報がないとアイテムは保存できない" do
-        @item.condition_id = ""
+        @item.condition_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Condition can't be blank")
+        expect(@item.errors.full_messages).to include("Condition must be other than 1")
       end
+
+
+
       it "・・・商品の状態の情報は1以外でないと登録できない" do
-        @item.condition_id = "1"
+        @item.condition_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Condition can't be blank")
+        expect(@item.errors.full_messages).to include("Condition must be other than 1")
       end
       
       it "配送料の負担の情報がないとアイテムは保存できない" do
-        @item.pay_id = ""
+        @item.pay_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Pay can't be blank")
+        expect(@item.errors.full_messages).to include("Pay must be other than 1")
       end
       it "・・・配送料の負担の情報は1以外でないと登録できない" do
-        @item.pay_id = "1"
+        @item.pay_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Pay can't be blank")
+        expect(@item.errors.full_messages).to include("Pay must be other than 1")
       end
       it "発送元の地域の情報がないとアイテムは保存できない" do
-        @item.area_id = ""
+        @item.area_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Area can't be blank")
+        expect(@item.errors.full_messages).to include("Area must be other than 1")
       end
       it "・・・発送元の地域の情報は1以外でないと登録できない" do
-        @item.area_id = "1"
+        @item.area_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Area can't be blank")
+        expect(@item.errors.full_messages).to include("Area must be other than 1")
       end
 
       it "発送までの日数の情報がないとアイテムは保存できない" do
-        @item.day_id = ""
+        @item.day_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Day can't be blank")
+        expect(@item.errors.full_messages).to include("Day must be other than 1")
       end
       it "・・・発送までの日数は1以外でないと登録できない" do
-        @item.day_id = "1"
+        @item.day_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Day can't be blank")
+        expect(@item.errors.full_messages).to include("Day must be other than 1")
       end
 
       it "価格の情報がないとアイテムは保存できない" do
         @item.price = ""
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price can't be blank")
+        expect(@item.errors.full_messages).to include("Price is not a number")
       end
       it "価格の範囲がの¥300~¥9,999,999の間でないとアイテムは保存できない" do
-        @item.price = "1"
+        @item.price = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
       end
       it "価格が299円以下だと出品できない" do
-        @item.price = "1"
+        @item.price = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
       end
       it "価格が10000000円以上だと出品できない" do
-        @item.price = "10000001"
+        @item.price = 10000001
         @item.valid?
         expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
       end

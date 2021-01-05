40b27b3f30c16aae6711b0138191db4f3aa3f689
icoicounday <75007893+icoicounday@users.noreply.github.com>
2021-01-05 18:25:59 +0900

    ""を外した

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -3,11 +3,11 @@ FactoryBot.define do
   factory :item do
     name {Faker::Name.name}
     text {"1234"}
-    category_id {"1234"}
-    condition_id {"1234"}
-    pay_id {"1234"}
-    area_id {"1234"}
-    day_id {"1234"}
+    category_id {1234}
+    condition_id {1234}
+    pay_id {1234}
+    area_id {1234}
+    day_id {1234}
     price{11111111}
     association :user
 

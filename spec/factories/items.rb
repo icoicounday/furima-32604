835e07300e13709db97042cb506de2ef9c420c26
icoicounday <75007893+icoicounday@users.noreply.github.com>
2021-01-05 20:08:54 +0900

    プルダウンの中を選択するためid:2を記述

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -3,12 +3,12 @@ FactoryBot.define do
   factory :item do
     name {Faker::Name.name}
     text {"1234"}
-    category_id {1234}
-    condition_id {1234}
-    pay_id {1234}
-    area_id {1234}
-    day_id {1234}
-    price{11111111}
+    category_id {2}
+    condition_id {2}
+    pay_id {2}
+    area_id {2}
+    day_id {2}
+    price{1111111}
     association :user
 
     after(:build) do |item|

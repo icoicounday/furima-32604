2644e8411353bfe4cf62b7aaba84162523f448d2
icoicounday <75007893+icoicounday@users.noreply.github.com>
2021-01-05 14:55:41 +0900

    バリデーションを追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,10 +4,13 @@ class Item < ApplicationRecord
   has_one_attached :image
   
   with_options presence: true do
+    validates :name
+    validates :text
     validates :category_id
-    validates :area_id
     validates :condition_id
     validates :pay_id, format: { with: /\A{1,7}[0-9\d]+\z/ }
+    validates :area_id
     validates :day_id
+    validates :price
   end
 end

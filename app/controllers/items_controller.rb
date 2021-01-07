c624dff7e407e0de477319fd9108ed05074d7906
icoicounday <75007893+icoicounday@users.noreply.github.com>
2021-01-07 16:19:38 +0900

    条件分岐を修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,7 +5,11 @@ class ItemsController < ApplicationController
   end
 
   def new
-    @item = Item.new
+    if user_signed_in?
+      @item = Item.new
+    else
+      redirect_to user_session_path(@item.id)
+    end
   end
 
 

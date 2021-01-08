a6dcb63d619f0508154386ac6f8647708a5f2ba8
icoicounday <75007893+icoicounday@users.noreply.github.com>
2021-01-08 16:12:30 +0900

    複数のアクションをまとめた

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show,]
+  before_action :set_action, only: [:edit, :show, :update,]
 
   def index
     @items = Item.order("created_at DESC")
@@ -19,16 +20,16 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+    # @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
+    # @item = Item.find(params[:id])
     redirect_to root_path unless current_user.id == @item.user_id
   end
 
   def update
-    @item = Item.find(params[:id])
+    # @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to root_path
     else
@@ -41,5 +42,8 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:name, :text, :category_id, :condition_id, :pay_id, :area_id, :day_id, :price, :user, :image).merge(user_id: current_user.id)
   end
+
+  def set_action
+    @item = Item.find(params[:id])
+  end
 end
- 

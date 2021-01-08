fa0833ccd0568db5a76b52530b07588130c68cea
icoicounday <75007893+icoicounday@users.noreply.github.com>
2021-01-08 10:59:52 +0900

    コントローラーにeditとupdateを追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,8 +5,8 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
-
 
   def create
     @item = Item.new(item_params)
@@ -19,6 +19,19 @@ class ItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    item = Item.find(params[:id])
+    if item.update(item_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
 
   private

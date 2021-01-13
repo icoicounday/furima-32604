a2b89d518f3e46562e29fd9562bf2b8896cda7f9
icoicounday <75007893+icoicounday@users.noreply.github.com>
2021-01-13 15:01:29 +0900

    メソッドにまとめてbefore_actionで呼び出すように修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,13 +1,12 @@
 class OrdersController < ApplicationController
+  before_action :set_action
+
   def index
-    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new
   end
 
   def create
-    # binding.pry
     @order_address = OrderAddress.new(order_params)
-    @item = Item.find(params[:item_id])
     if @order_address.valid?
        pay_item
        @order_address.save
@@ -30,5 +29,9 @@ class OrdersController < ApplicationController
       card: order_params[:token],    # カードトークン
       currency: 'jpy'                 # 通貨の種類（日本円）
     )
+  end
+
+  def set_action
+    @item = Item.find(params[:item_id])
   end
 end

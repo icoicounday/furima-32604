bdf464e4aca497f7217bd89f3c68f347200e3a23
icoicounday <75007893+icoicounday@users.noreply.github.com>
2021-01-13 12:17:50 +0900

    step2手順1/2/3トークンに対するバリデーションを設定した

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,9 +5,11 @@ class OrdersController < ApplicationController
   end
 
   def create
+    # binding.pry
     @order_address = OrderAddress.new(order_params)
     @item = Item.find(params[:item_id])
     if @order_address.valid?
+       pay_item
        @order_address.save
       return redirect_to root_path
     else
@@ -18,7 +20,15 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order_address).permit(:post_number, :area_id,:city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
-    # item_idとuser_idを受け取る必要がある。user_idはこちらでmergeしてあげる必要がある。pictweetのcomments_controllerを参考に。
+    params.require(:order_address).permit(:post_number, :area_id,:city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id,], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = "sk_test_4a40ec329c6082363347d310"  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+    Payjp::Charge.create(
+      amount: @item.price,  # 商品の値段
+      card: order_params[:token],    # カードトークン
+      currency: 'jpy'                 # 通貨の種類（日本円）
+    )
   end
 end
--- app/javascript/price.js
+++ app/javascript/price.js
@@ -1,14 +1,52 @@
-window.addEventListener('load', () => {
+const pay = () => {
+  Payjp.setPublicKey("pk_test_958b5eb6f25617a39bde50d0"); // PAY.JPテスト公開鍵
+  const form = document.getElementById("charge-form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
 
-  const priceInput = document.getElementById("item-price");
-  priceInput.addEventListener("input", () => {
-    const inputValue = priceInput.value;
+    const formResult = document.getElementById("charge-form");
+    const formData = new FormData(formResult);
 
-    const addTaxDom = document.getElementById("add-tax-price")
-    addTaxDom.innerHTML = (Math.floor(inputValue * 0.1));
-    
-    const profitNumber = document.getElementById("profit")
-    const value_result = inputValue * 0.9
-    profitNumber.innerHTML = (Math.floor(value_result));
+    const card = {
+      number: formData.get("order_address[card_number]"),
+      cvc: formData.get("order_address[card_cvc]"),
+      exp_month: formData.get("order_address[card_exp_month]"),
+      exp_year: `20${formData.get("order_address[card_exp_year]")}`,
+    };
+    console.log(card)
+
+    Payjp.createToken(card, (status, response) => {
+      console.log("OK")
+      if (status == 200) {
+        const token = response.id;
+        const renderDom = document.getElementById("charge-form");
+        const tokenObj = `<input value=${token} name='token' type="hidden"> `;
+        renderDom.insertAdjacentHTML("beforeend", tokenObj);
+      }
+
+      document.getElementById("card-number").removeAttribute("name");
+      document.getElementById("card-cvc").removeAttribute("name");
+      document.getElementById("card-exp-month").removeAttribute("name");
+      document.getElementById("card-exp-year").removeAttribute("name");
+
+      document.getElementById("charge-form").submit();
+    });
   });
-});
+};
+
+// window.addEventListener('load', () => {
+
+//   const priceInput = document.getElementById("item-price");
+//   // priceInput.addEventListener("input", () => {
+//     const inputValue = priceInput.value;
+
+//     const addTaxDom = document.getElementById("add-tax-price")
+//     addTaxDom.innerHTML = (Math.floor(inputValue * 0.1));
+    
+//     const profitNumber = document.getElementById("profit")
+//     const value_result = inputValue * 0.9
+//     profitNumber.innerHTML = (Math.floor(value_result));
+//   });
+// });
+
+window.addEventListener("load", pay);

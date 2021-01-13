0b0268756b0b0783ce8e60fa541f7adcc36e6dd8
icoicounday <75007893+icoicounday@users.noreply.github.com>
2021-01-13 12:42:38 +0900

    step2手順4 鍵情報を環境変数として設定した

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -24,7 +24,7 @@ class OrdersController < ApplicationController
   end
 
   def pay_item
-    Payjp.api_key = "sk_test_4a40ec329c6082363347d310"  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     Payjp::Charge.create(
       amount: @item.price,  # 商品の値段
       card: order_params[:token],    # カードトークン
--- app/javascript/price.js
+++ app/javascript/price.js
@@ -1,5 +1,5 @@
 const pay = () => {
-  Payjp.setPublicKey("pk_test_958b5eb6f25617a39bde50d0"); // PAY.JPテスト公開鍵
+  Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
   const form = document.getElementById("charge-form");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -33,20 +33,5 @@ const pay = () => {
     });
   });
 };
-
-// window.addEventListener('load', () => {
-
-//   const priceInput = document.getElementById("item-price");
-//   // priceInput.addEventListener("input", () => {
-//     const inputValue = priceInput.value;
-
-//     const addTaxDom = document.getElementById("add-tax-price")
-//     addTaxDom.innerHTML = (Math.floor(inputValue * 0.1));
-    
-//     const profitNumber = document.getElementById("profit")
-//     const value_result = inputValue * 0.9
-//     profitNumber.innerHTML = (Math.floor(value_result));
-//   });
-// });
 
 window.addEventListener("load", pay);
